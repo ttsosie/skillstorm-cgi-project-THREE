bf13fb0670862550b337eabd6584a7c27082b347
ttsosie <tiffiny.tsosie@mykenzie.snhu.edu>
2025-08-20 00:35:10 -0700

    Question and Quiz DTO to match updates in respective models where the null-ables match what DB has with wrapper classes Integer

--- quiz-api/src/main/java/com/skillstorm/dtos/QuestionDTO.java
+++ quiz-api/src/main/java/com/skillstorm/dtos/QuestionDTO.java
@@ -2,6 +2,6 @@ package com.skillstorm.dtos;
 
 import com.skillstorm.quiz_models.Question;
 
-public record QuestionDTO(int id, int quizId, int sequence, String description, String type) {
+public record QuestionDTO(int id, Integer quizId, Integer sequence, String description, String type) {
 
 }
--- quiz-api/src/main/java/com/skillstorm/dtos/QuizDTO.java
+++ quiz-api/src/main/java/com/skillstorm/dtos/QuizDTO.java
@@ -2,6 +2,6 @@ package com.skillstorm.dtos;
 
 import com.skillstorm.quiz_models.Quiz;
 
-public record QuizDTO(int id, String name, String subject, String description, int numberOfQuestions, String quizImageSrc) {
+public record QuizDTO(int id, String name, String subject, String description, Integer numberOfQuestions, String quizImageSrc) {
 
 }
